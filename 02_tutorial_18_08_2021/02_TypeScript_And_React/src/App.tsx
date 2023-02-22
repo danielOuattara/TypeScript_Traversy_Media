1231a868207cc57f2760811cf50adec38c5d1f27
danielOuattara <daniel.ouattara@gmx.com>
2023-02-22 21:49:18 +0100

    TypeScript and React

---
+++ 02_tutorial_18_08_2021/02_TypeScript_And_React/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header title="React TypeScript" color="green" />
+    </div>
+  );
+}
+
+export default App;
